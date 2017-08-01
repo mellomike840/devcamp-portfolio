fabde16698c9e3ef3d43336f239cb3a373433fa1
Jordan Hudgens <jordan@devcamp.com>
2017-08-01 17:08:50 +0000

    Implemented initial file uploading system via carierwave for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -132,24 +132,17 @@ class PortfoliosController < ApplicationController
 
   def portfolio_params
 
-    params.require(:portfolio).permit(:title,
-
+    params.require(:portfolio).permit(
+                                      :title,
                                       :subtitle,
-
                                       :body,
-
+                                      :main_image,
+                                      :thumb_image,
                                       technologies_attributes: [:name]
-
-                                     )
-
+                                      )
   end
-
-
 
   def set_portfolio_item
-
     @portfolio_item = Portfolio.find(params[:id])
-
   end
-
 end
