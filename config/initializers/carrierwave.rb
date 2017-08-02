8d88d82b46e1df61580e166d9d00dd211d9dcf7e
Jordan Hudgens <jordan@devcamp.com>
2017-08-02 16:14:53 +0000

    Updated placeholder content for blogs

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,9 +7,9 @@ CarrierWave.configure do |config|
     expires: 1.week.from_now.httpdate,
     cache_control: 'max-age=604800'
   }
-
+ 
   config.aws_credentials = {
-    access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+    access_key_id:     ENV.fetch('AWS_ACCESS_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     region:            ENV.fetch('AWS_REGION') # Required
   }
